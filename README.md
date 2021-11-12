1b9e5e678875d090edebaa2c661f25efd49fdc6d
Mitchel Humpherys <mitch.special@gmail.com>
2021-11-12 12:04:52 -0800

    Initial implementation

---
+++ README.md
@@ -0,0 +1,4 @@
+# DuckDuckGoog
+
+This is a Firefox addon to put g! on a keyboard shortcut for quicker DDG
+ripcord pulling.
---
+++ duckduckgoog.js
@@ -0,0 +1,13 @@
+const pullRipcord = () => {
+    const ipt = document.getElementById("search_form_input");
+    ipt.value += ' g!';
+    ipt.form.submit();
+};
+
+browser.commands.onCommand.addListener((command) => {
+    console.log("Got command:", command);
+    if (command === "pull-ripcord")
+        pullRipcord();
+});
+
+console.log("HELLO DUCKDUCKGOOG");
